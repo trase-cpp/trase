09a35505aab686ccf7b1428b078f9a0eadd3f916
Martin Robinson <martinjrobins@gmail.com>
2018-06-12 06:38:13 +0100

    #3 fix stationary ticks

--- src/AxisDraw.hpp
+++ src/AxisDraw.hpp
@@ -97,17 +97,17 @@ template <typename Backend> void Axis::draw(Backend &backend) {
 
   // ticks
   const int ny_ticks = 5;
-  const int sig_digits = 3;
+  const int sig_digits = 2;
 
   // calculate y tick locations
-  const float tick_min_y = round_off(ymin, sig_digits);
+  const float tick_min_y = round_off(ymin, 1);
   const float tick_dx_y = round_off(yh / ny_ticks, sig_digits);
   // scale to pixels
   const float tick_dx_y_pixels = tick_dx_y * h / yh;
   const float tick_min_y_pixels = y + h * (1 - (tick_min_y - ymin) / yh);
 
   // calculate x tick locations
-  const float tick_min_x = round_off(xmin, sig_digits);
+  const float tick_min_x = round_off(xmin, 1);
   const int nx_ticks = w / tick_dx_y_pixels;
   const float tick_dx_x = round_off(xw / nx_ticks, sig_digits);
   // scale to pixels
@@ -129,16 +129,16 @@ template <typename Backend> void Axis::draw(Backend &backend) {
     const float tick_val = tick_min_x + (i + 0.5) * tick_dx_x;
     backend.move_to(tick_pos, y + h + tick_len / 2);
     backend.line_to(tick_pos, y + h - tick_len / 2);
-    std::sprintf(buffer, "%1.2f", tick_val);
+    std::sprintf(buffer, "%.*g", sig_digits, tick_val);
     backend.text(tick_pos, y + h + tick_len / 2, buffer, NULL);
   }
   backend.text_align(ALIGN_RIGHT | ALIGN_MIDDLE);
   for (int i = 0; i < ny_ticks; ++i) {
-    const float tick_pos = tick_min_y_pixels + h - (i + 0.5) * tick_dx_y_pixels;
+    const float tick_pos = tick_min_y_pixels - (i + 0.5) * tick_dx_y_pixels;
     const float tick_val = tick_min_y + (i + 0.5) * tick_dx_y;
     backend.move_to(x - tick_len / 2, tick_pos);
     backend.line_to(x + tick_len / 2, tick_pos);
-    std::sprintf(buffer, "%1.2f", tick_val);
+    std::sprintf(buffer, "%.*g", sig_digits, tick_val);
     backend.text(x - tick_len / 2, tick_pos, buffer, NULL);
   }
   backend.stroke_color(RGBA(0, 0, 0, 255));
