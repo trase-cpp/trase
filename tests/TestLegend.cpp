8b3acfbd8ad677d1f137c3536b6d0bf25f9dec4d
Martin Robinson <martinjrobins@gmail.com>
2018-10-11 09:48:16 +0100

    #62 add legend test

---
+++ tests/TestLegend.cpp
@@ -0,0 +1,69 @@
+/*
+Copyright (c) 2018, University of Oxford.
+All rights reserved.
+
+University of Oxford means the Chancellor, Masters and Scholars of the
+University of Oxford, having an administrative office at Wellington
+Square, Oxford OX1 2JD, UK.
+
+This file is part of the Oxford RSE C++ Template project.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "catch.hpp"
+
+#include "DummyDraw.hpp"
+
+#include "trase.hpp"
+#include <fstream>
+
+using namespace trase;
+
+TEST_CASE("legend creation", "[legend]") {
+  auto fig = figure();
+  auto ax = fig->axis();
+  const int n = 100;
+  std::vector<float> x(n);
+  std::vector<float> y(n);
+  for (int i = 0; i < n; ++i) {
+    x[i] = 6.28f * static_cast<float>(i) / n;
+    y[i] = std::sin(x[i]);
+  }
+  auto data = create_data().x(x).y(y);
+  ax->histogram(data);
+  ax->line(data);
+  ax->points(data);
+  DummyDraw::draw("histogram", fig);
+  x.resize(1);
+  x[0] = 0.1f;
+  ax->histogram(create_data().x(x));
+  DummyDraw::draw("histogram", fig);
+  x.resize(5);
+  x[1] = 0.2f;
+  x[2] = 0.3f;
+  x[3] = 0.4f;
+  x[4] = 0.5f;
+  ax->histogram(create_data().x(x));
+  DummyDraw::draw("histogram", fig);
+}
