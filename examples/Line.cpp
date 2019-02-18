e3747c1dcf1b83b117265268c4b17012f753ffee
Martin Robinson <martinjrobins@gmail.com>
2019-02-18 09:35:57 +0000

    #118 update line image

--- examples/Line.cpp
+++ examples/Line.cpp
@@ -34,8 +34,9 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 /// \page example_line Line Geometry
 ///  This is an example for the line geometry
 ///
-/// \image html example_line.svg "Output"
-/// \snippet examples/Line.cpp example line
+/// \image html
+/// https://rawgit.com/trase-cpp/generated_files/master/example_line.svg
+/// "Output" \snippet examples/Line.cpp example line
 
 /// [example line]
 #include "trase.hpp"
